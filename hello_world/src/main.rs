4b14cf1f335a486c81702d2a079474a962f255d7
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-05 22:22:00 +0530

    Match two values

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,6 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Hello, world!");
@@ -12,7 +13,7 @@ fn guessing_game() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
-    
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -21,5 +22,13 @@ fn guessing_game() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
