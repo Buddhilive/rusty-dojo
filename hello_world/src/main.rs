c2ffa3e9fdc1085ffa29ab3307ba87419aecdf49
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-19 22:22:00 +0530

    Handle wrong data input

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -23,7 +23,10 @@ fn guessing_game() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
