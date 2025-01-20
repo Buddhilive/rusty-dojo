803660f697dcf1b412664fed34ca5b0a08a62ed3
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-01-20 22:22:00 +0530

    Finalize the game

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 fn guessing_game() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
